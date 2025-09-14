cefa82a4627d9a2456f3f904ad7fe9ddea7b3163
Guo61 <gg1457o@qq.com>
2025-09-14 17:23:44 +0800

    Add files via upload

--- 自然灾害.lua
+++ 自然灾害.lua
@@ -1,4 +1,4 @@
-local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Guo61/Cat-/refs/heads/main/main.lua"))()
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local Confirmed = false
 
